6043af488bd65ba13f7f70f1ad3d6c046e62887f
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-09-29 01:51:33 +0200

    Testing getAllStudents

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,31 +4,46 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
 
-    @Autowired
+//    @Autowired
+    @Mock
     private StudentRepository studentRepository;
     private StudentService studentService;
+//    private AutoCloseable autoCloseable;
+
 
     @BeforeEach
     void setUp() {
+//        autoCloseable = MockitoAnnotations.openMocks(this);
         studentService = new StudentService(studentRepository);
-
     }
 
-    @AfterEach
-    void tearDown() {
-
-    }
+//    @AfterEach
+//    void tearDown() throws Exception {
+//        autoCloseable.close();
+//    }
 
     @Test
     void getAllStudents() {
 
+        //when
+        studentService.getAllStudents();
+
+        //then
+        verify(studentRepository).findAll();//Passed
+//        verify(studentRepository).deleteAll();///Failed
 
     }
 
